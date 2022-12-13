d5d76db9b8f11b86b0fb3d4990b2a4999cfcbac0
ionasloan <66360926+ionasloan@users.noreply.github.com>
2022-12-13 20:51:53 +0000

    Adding Styling & Updating Folder Structure-Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import Events from './components/Events/Events';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+import useStyles from './styles';
+
 
 const App = () =>{
     return(
