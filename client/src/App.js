c74f0bc8ccfcf5e6f5f32c2fb8d5b49d0ac8d952
ionasloan <66360926+ionasloan@users.noreply.github.com>
2022-12-13 20:52:08 +0000

    Styling & Folder structure working

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,29 @@ import useStyles from './styles';
 
 
 const App = () =>{
+
+    const classes = useStyles();
+
     return(
-    <div>
-        <h1>Hello</h1>
-    </div>);  
+    <Container maxwidth ="lg">
+        <AppBar className={classes.appBar} position="static" colour="inherit">
+            <Typography className={classes.heading} varient="h2" align="center">Memories</Typography>
+            <img src={memories} alt="memories" height="60" />
+        </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing="{3}">
+                    <Grid item xs={12} sm={7}>
+                        <Events />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    </Container>
+    );
 }
 
 export default App;
--- server/controllers/events.js
+++ server/controllers/events.js
@@ -14,6 +14,17 @@ export const getEvents =  async (req, res) => {
 }
 
 
-export const createEvent = (req, res) =>{
+export const createEvent = async(req, res) =>{
     //res.send('Event Creation');
+    const event = req.body;
+
+    const newEvent = new EventListing(event);
+
+    try{
+       await newEvent.save();
+        res.status(201).json(newEvent);
+       }
+       catch(error){
+        res.status(409).json({message: error.message});
+       }
 }
